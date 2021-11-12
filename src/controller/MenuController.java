7b94baa771d5b17f2664962f46f76af8babb2ad1
CredoSpero <64059669+CredoSpero@users.noreply.github.com>
2021-11-13 00:19:35 +0800

    Made changes for demonstration

--- src/controller/MenuController.java
+++ src/controller/MenuController.java
@@ -133,7 +133,7 @@ public class MenuController extends AbstractController {
         int choice=0;
         int idx;
         while (choice != 4) {
-            System.out.println("============== Manage this set ==============\n1. Add item \n2. Remove item \n3. Display the items in the set \n4. Finish");
+            System.out.println("============== Manage this set ==============\n1. Add item \n2. Remove item \n3. Display set \n4. Finish");
             while (true) {
                 try {
                     System.out.println("Enter choice: ");
@@ -204,7 +204,11 @@ public class MenuController extends AbstractController {
                     System.out.println("AlaCarte Item successfully removed!");
                     break;
                 case 3:
+                    System.out.println("\n============== Displaying Set ==============");
+                    System.out.println(setItem.toString());
                     setItem.displayItemsInSet();
+                    break;
+                case 4:
                     break;
                 default:
                     System.out.println("Invalid input");
@@ -239,13 +243,13 @@ public class MenuController extends AbstractController {
                 case 1:
                     System.out.println("Enter new name: ");
                     mItem.setName(sc.nextLine());
-                    System.out.println("Updated set: ");
+                    System.out.println("Updated Set: ");
                     System.out.println(mItem.toString());
                     break;
                 case 2:
                     System.out.println("Enter new description: ");
                     mItem.setDescription(sc.nextLine());
-                    System.out.println("Updated set: ");
+                    System.out.println("Updated Set: ");
                     System.out.println(mItem.toString());
                     break;
                 case 3:
@@ -283,7 +287,7 @@ public class MenuController extends AbstractController {
     public void updateAlaCarte(MenuItem aCarte){
         int choice = 0;
         while (choice != 5) {
-            System.out.println("============== Update this ala carte ==============\n1. Update name \n2. Update description \n3. Update price \n4. Display ala carte \n5. Finish");
+            System.out.println("============== Update this Ala Carte ==============\n1. Update name \n2. Update description \n3. Update price \n4. Display ala carte \n5. Finish");
             while (true) {
                 try {
                     System.out.println("Enter choice: ");
@@ -301,13 +305,15 @@ public class MenuController extends AbstractController {
                 case 1:
                     System.out.println("Enter new name: ");
                     aCarte.setName(sc.nextLine());
-                    System.out.println("Updated ala carte: ");
+                    System.out.println();
+                    System.out.println("Updated AlaCarte: ");
                     System.out.println(aCarte.toString());
                     break;
                 case 2:
                     System.out.println("Enter new description: ");
                     aCarte.setDescription(sc.nextLine());
-                    System.out.println("Updated ala carte: ");
+                    System.out.println();
+                    System.out.println("Updated AlaCarte: ");
                     System.out.println(aCarte.toString());
                     break;
                 case 3:
@@ -325,6 +331,7 @@ public class MenuController extends AbstractController {
                         }
                         break;
                     }
+                    System.out.println();
                     System.out.println("Updated ala carte: ");
                     System.out.println(aCarte.toString());
                     for (MenuItem m: menuList) {
@@ -335,6 +342,8 @@ public class MenuController extends AbstractController {
                     break;
                 case 4:
                     System.out.println(aCarte.toString());
+                    break;
+                case 5: 
                     break;
                 default:
                     System.out.println("Invalid input");
